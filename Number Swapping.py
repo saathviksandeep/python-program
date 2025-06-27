fbaf3e278a5ca59e3c5ab511082e6c0d6c08ceb4
saathviksandeep <saathvik3012@gmail.com>
2025-06-26 19:28:00 -0700

    Number Swapping

---
+++ Number Swapping.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter the first number: "))
+
+num2=int(input("Enter the second number: "))
+
+print("The value of the first number before swapping is: ", num1)
+
+print("The value of the second number before swapping is: ", num2)
+
+num1,num2=num2,num1
+
+print("The value of the first number after swapping is: ", num1)
+
+print("The value of the second number after swapping is: ", num2)
